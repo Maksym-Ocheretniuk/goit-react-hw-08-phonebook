b46a48889601e9f34a045728357220f940a06979
Maksym Ocheretniuk <max.ocheret@gmail.com>
2023-10-02 16:16:46 +0300

    RegisterForm

--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -1,7 +1,10 @@
+import { RegisterForm } from 'components/RegisterForm/RegisterForm';
+
 export const Register = () => {
   return (
     <>
       <h1>Registration</h1>
+      {<RegisterForm />}
     </>
   );
 };
