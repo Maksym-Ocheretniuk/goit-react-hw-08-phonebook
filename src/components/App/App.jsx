0a9ffadb80fb181d41b16e5ac4f64cacf87b2857
Maksym Ocheretniuk <max.ocheret@gmail.com>
2023-10-17 23:38:14 +0300

    App AppBar style

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -7,11 +7,9 @@ import { useDispatch } from 'react-redux';
 // import Filter from './Filter/Filter';
 // import ContactForm from './ContactForm/ContactForm';
 
-import css from './App.module.css';
-
 import { Layout } from '../Layout/Layout';
 import Home from 'pages/Home/Home';
-import Register from 'pages/Register/Register';
+import { Register } from 'pages/Register/Register';
 import { Login } from 'pages/Login/Login';
 import Contacts from 'pages/Contacts/Contacts';
 import Loader from 'components/Loader/Loader';
@@ -19,6 +17,8 @@ import { RestrictedRoute } from 'components/RestrictedRoute/RestrictedRoute';
 import { PrivateRoute } from 'components/PrivateRoute/PrivateRoute';
 import { useAuth } from 'hooks';
 import { refreshUser } from 'redux/auth/operations';
+
+import css from './App.module.css';
 
 export const App = () => {
   const dispatch = useDispatch();
@@ -29,33 +29,38 @@ export const App = () => {
   }, [dispatch]);
 
   return isRefreshing ? (
-    <div className={css.loaderWrap}>
+    <div className={css.loaderContainer}>
       <Loader />
     </div>
   ) : (
-    <Routes>
-      <Route path="/" element={<Layout />}>
-        <Route index element={<Home />} />
-        <Route
-          path="/register"
-          element={
-            <RestrictedRoute redirectTo="/contacts" component={<Register />} />
-          }
-        />
-        <Route
-          path="/login"
-          element={
-            <RestrictedRoute redirectTo="/contacts" component={<Login />} />
-          }
-        />
-        <Route
-          path="/contacts"
-          element={
-            <PrivateRoute redirectTo="/login" component={<Contacts />} />
-          }
-        />
-      </Route>
-    </Routes>
+    <div className={css.mainContainer}>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route
+            path="/register"
+            element={
+              <RestrictedRoute
+                redirectTo="/contacts"
+                component={<Register />}
+              />
+            }
+          />
+          <Route
+            path="/login"
+            element={
+              <RestrictedRoute redirectTo="/contacts" component={<Login />} />
+            }
+          />
+          <Route
+            path="/contacts"
+            element={
+              <PrivateRoute redirectTo="/login" component={<Contacts />} />
+            }
+          />
+        </Route>
+      </Routes>
+    </div>
   );
 };
 
