a7384ff5ca0eecb3acc66d9e6e126135735c68a0
Maksym Ocheretniuk <max.ocheret@gmail.com>
2023-10-20 15:30:21 +0300

    Contacts (page) style

--- src/pages/Contacts/Contacts.jsx
+++ src/pages/Contacts/Contacts.jsx
@@ -1,17 +1,19 @@
+import { useDispatch, useSelector } from 'react-redux';
+import { Toaster } from 'react-hot-toast';
+import { useEffect } from 'react';
 import { ContactForm } from '../../components/ContactForm/ContactForm';
 import { Filter } from '../../components/Filter/Filter';
 import { ContactList } from '../../components/ContactList/ContactList';
-import { useDispatch, useSelector } from 'react-redux';
 import {
   selectContactsCount,
   selectIsLoading,
   selectError,
 } from 'redux/contacts/selectors';
-import { Toaster } from 'react-hot-toast';
-import { useEffect } from 'react';
 import { fetchContacts } from 'redux/contacts/operations';
 import Loader from 'components/Loader/Loader';
 // import { Loader } from '../../components/Loader/Loader';
+
+import css from './Contacts.module.css';
 
 export default function App() {
   const dispatch = useDispatch();
@@ -24,17 +26,17 @@ export default function App() {
   }, [dispatch]);
 
   return (
-    <div>
-      <h1>Phonebook</h1>
+    <div className={css.container}>
+      <h1 className={css.title}>Phonebook</h1>
       <ContactForm />
-      <h2>Contacts</h2>
-      <p>
+      <h2 className={css.subtitle}>Contacts</h2>
+      <p className={css.total}>
         Total contacts in phonebook:
-        <span> {count}</span>
+        <span className={css.totalNumber}> {count}</span>
       </p>
       <Filter />
       {isLoading && !error && <Loader />}
-      {error && <p>An error occurred: {error}</p>}
+      {error && <p className={css.errorMessage}>An error occurred: {error}</p>}
       <ContactList />
       <Toaster position="top-right" />
     </div>
