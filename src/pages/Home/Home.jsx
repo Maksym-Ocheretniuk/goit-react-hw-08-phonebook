cdf68dbed865e5dac1117d7a69304f2b72a540f9
Maksym Ocheretniuk <max.ocheret@gmail.com>
2023-10-20 18:36:58 +0300

    upd background-image

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,7 +3,6 @@ import css from './Home.module.css';
 export default function Home() {
   return (
     <div className={css.container}>
-      <div className={css.bgImage}></div>
       <h1 className={css.title}>Phonebook</h1>
     </div>
   );
